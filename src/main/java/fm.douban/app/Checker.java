1e7f06adda745ccfe5e33f767cc6288e56233f51
GreyJY <3224282509@qq.com>
2026-08-19 03:58:50 +0800

    Update Checker.java

--- src/main/java/fm.douban/app/Checker.java
+++ src/main/java/fm.douban/app/Checker.java
@@ -1,1 +1,33 @@
+package fm.douban.app;
 
+import java.io.*;
+
+import org.spriingframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+@Service 
+  public class Checker implements InitializingBean/** 这里的接口是spring自己提供的，接口实现类需要重写*/{
+@Override
+    public void afterPropertiesSet()throws Exception{
+Thread thread = new Thread(new Runnable(){
+  @Override
+  public void run(){
+try{
+  Thread.sleep(6000);
+    }catch(InterruptedException e){
+  e.printStackTrace();
+    }
+try{
+  System.exit(0);
+}catch(Exception e){
+  
+}
+    
+  }
+});
+thread.start();
+      
+    }
+
+    
+  }
