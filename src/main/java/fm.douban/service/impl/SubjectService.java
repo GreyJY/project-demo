b371eb71f795ce4f3fdbb273a53cd89213a4654c
GreyJY <3224282509@qq.com>
2026-08-19 23:27:14 +0800

    Update SubjectService.java

--- src/main/java/fm.douban/service/impl/SubjectService.java
+++ src/main/java/fm.douban/service/impl/SubjectService.java
@@ -1,1 +1,38 @@
+package fm.douban.service.impl;
+
+import fm.douban.model.Song;
+import fm.douban.model.Subject;
+import fm.douban.service.SongService;
+import fm.douban.service.SubjectService;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//依旧业务逻辑层
+@Service
+public class SubjectServiceImpl implements SubjectService {
+    private songService songService;
+
+    //缓存所有专辑数据
+    private static Map<String, Subject> subjectMap = new HashMap<>();
+    static {
+        Subject subject = new Subject();
+        subject.setId("s001");
+        subjectMap.put(subject.getId(), subject);
+    }
+
+    @Override
+    public Subject get(String subjectId) {
+        Subject subject = subjectMap.get(subjectId);
+        //调用 SongService 获取专辑歌曲
+        List<Song> songs = songService.list(subjectId);
+        subject.setSongs(songs);
+        return subject;
+    }
+
+    public void setSongService(SongService songService) {
+        this.songService = songService;
+    }
+
+}
 
