545d7c432b043275995d631c454396a30da35f49
GreyJY <3224282509@qq.com>
2026-08-19 22:48:52 +0800

    Update UserLoginInfo.java

--- src/main/java/fm.douban/model/UserLoginInfo.java
+++ src/main/java/fm.douban/model/UserLoginInfo.java
@@ -1,1 +1,29 @@
+//这个则是登陆信息有关的模型数据对象
+package fm.douban.model;
+
+import java.io.Serializable;
+
+/**
+ * 演示用的用户登录信息
+ */
+public class UserLoginInfo implements Serializable {
+    private String userId;
+    private String userName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
 
