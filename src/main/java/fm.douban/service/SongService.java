7f53b5e306c8ad7b2784875e31509bf23a3dc81b
GreyJY <3224282509@qq.com>
2026-08-19 22:52:08 +0800

    Update SongService.java

--- src/main/java/fm.douban/service/SongService.java
+++ src/main/java/fm.douban/service/SongService.java
@@ -1,1 +1,15 @@
+//前面都是为了接收信息用的方法，而这个service里面的方法是实现业务逻辑的，这是个接口，为了解耦合
 
+package fm.douban.service;
+
+import fm.douban.model.Song;
+
+import java.util.List;
+
+public interface SongService {
+
+    Song get(String songId);
+
+    List<Song> list(String subjectId);
+
+}
